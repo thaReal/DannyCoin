59d11fdf5360aabe7f3872d8fbcf6c3b16ba7f3d
thaReal <daniel.ruland@gmail.com>
2015-12-22 15:28:52 -0500

    forgot to fix initial checkpoint; testnet should be good to go

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -26,13 +26,7 @@ namespace Checkpoints
     //
     static MapCheckpoints mapCheckpoints =
             boost::assign::map_list_of
-            (     0, uint256("0x0a59605118489aa9cde58e64210d3d56e246758fbabcd1482e54bf4032545221"))
-			(     1000, uint256("0xebce88a0891a2b4cd24c9e1baace8381bb38963a603d5ac4ec67f85b2bbb3838"))
-			(     5000, uint256("0xdb8b18987a8af6fb5e7df2cedf2ba8eaa8c26990b93b82fe05cd1cd2656211a2"))
-			(     8000, uint256("0xc6e73ae9841cc79cdd825d5b5faadf3be6c4312b2ddb19f29ab8133d4b3cd679"))
-			(     35120, uint256("0xccb94eddd81fb5dcbedd6b503f0ed2291c494c72a844447d8edd94ea8f7bff28"))
-			(     276000, uint256("0xd187fa6c813cafd086c3e653f29acdd7eee87e053d8c3724a6e35950a8339734"))
-			(     312200, uint256("0x3f86beee80b99d469857b7d411469efe5b3c418287ba993df6eb235a95db92c7"))
+            (     0, uint256("0x067c700cb42ba78726c345d906bbfc0dd485a3426b9d728f5a7947fe4629ddda"))
             ;
 
 
